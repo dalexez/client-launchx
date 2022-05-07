8991060cac3fe206465049c38fd30b8b60c4c25b
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-05-07 17:57:25 -0500

    Modificacion de nombres de rutas

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,13 @@
           <router-link to="/explorers" class="nav-link">Explorers</router-link>
         </li>
         <li class="nav-item">
-          <router-link to="/add" class="nav-link">Agregar Explorer</router-link>
+          <router-link to="/addExplorer" class="nav-link">Agregar Explorer</router-link>
         </li>
         <li class="nav-item">
           <router-link to="/missioncommander" class="nav-link">Mission Commander</router-link>
         </li>
         <li class="nav-item">
-          <router-link to="/add" class="nav-link">Agregar Mission Commander</router-link>
+          <router-link to="/addMissionCommander" class="nav-link">Agregar Mission Commander</router-link>
         </li>
       </div>
     </nav>
