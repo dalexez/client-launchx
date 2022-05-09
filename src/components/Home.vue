a785adb2388d2085d0e03a2ef8bd6a5379a2370a
dalexez <dalexez@gmail.com>
2022-05-09 12:47:58 -0500

    Agregando imagen de home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-        HOLA PRUEBA
+        <img src="../images/launchx.png" alt="Cartel Bienvenida">
     </div>
 </template>
 
 <style scoped>
 div {
+    display: flex;
     width: 100%;
     justify-content: center;
+}
+img {
+    width: 100%;
 }
 </style>
